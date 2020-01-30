cb848694b3efd660e68bc492258e417de4ad820b
Svetlana <vashchenko.siu@students.dvfu.ru>
2020-01-30 20:04:33 +1000

    init

---
+++ Gradient/Gradient.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 6e284fc32efed124920712aa0db6b3b62f65cdf9 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1421
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372303
+	.include	"typemap.mj.inc"
---
+++ Gradient/Gradient/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+namespace Gradient.CustomControls
+{
+    public class GradientColorStack : StackLayout
+    {
+        public Color StartColor
+        {
+            get;
+            set;
+        }
+        public Color EndColor
+        {
+            get;
+            set;
+        }
+    }
+}
